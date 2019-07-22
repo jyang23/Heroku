de42bfc8c7678af59dc9fb8c67dae9427951bb0b
Justin <justinyang1000@hotmail.com>
2019-07-22 13:19:52 -0400

    Initial commit

---
+++ src/main/java/com/jy/hero/HomeController.java
@@ -0,0 +1,14 @@
+package com.jy.hero;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String hello()
+    {
+        return "hello";
+    }
+}
